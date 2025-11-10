19b36321a94f221fc726ce6e68611f2e28daaaf7
Bautista Cutini <bauticutinii@gmail.com>
2025-11-10 09:31:58 -0300

    Add files via upload

---
+++ TP3/punto_3.sql
@@ -0,0 +1,26 @@
+
+CREATE DATABASE IF NOT EXISTS programas_radio;
+USE programas_radio;
+-- ========================
+-- Tabla RADIO_ANIO
+-- ========================
+CREATE TABLE RADIO_ANIO (
+    radio VARCHAR(50) NOT NULL,
+    anio YEAR NOT NULL,
+    frecuencia_radio VARCHAR(20) NOT NULL,
+    gerente VARCHAR(100) NOT NULL,
+    PRIMARY KEY (radio, anio)
+);
+-- ========================
+-- Tabla PROGRAMA
+-- ========================
+CREATE TABLE PROGRAMA (
+    radio VARCHAR(50) NOT NULL,
+    anio YEAR NOT NULL,
+    programa VARCHAR(100) NOT NULL,
+    conductor VARCHAR(100) NOT NULL,
+    PRIMARY KEY (radio, anio, programa),
+    FOREIGN KEY (radio, anio) REFERENCES RADIO_ANIO(radio, anio)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
